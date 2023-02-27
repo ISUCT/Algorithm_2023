daaa2ee2ac0fd02811f29cc55305faeb3f598bdf
Fuflick <stepan_belov_2016@mail.ru>
2023-02-27 22:48:38 +0300

    fix some troubles

---
+++ CourseApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using CourseApp.Module2;
+
+namespace CourseApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BubbleSort.BubbleSortMethod();
+
+            Console.WriteLine("Hello World");
+        }
+    }
+}
