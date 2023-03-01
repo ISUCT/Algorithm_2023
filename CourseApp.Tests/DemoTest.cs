2cf05e573cb98ed4af8c2d27243984fe96b9dfba
Fuflick <stepan_belov_2016@mail.ru>
2023-03-02 01:42:36 +0300

    Tests for first task from Yandex Contest

--- CourseApp.Tests/DemoTest.cs
+++ CourseApp.Tests/DemoTest.cs
@@ -1,5 +1,6 @@
 using System;
 using Xunit;
+using CourseApp;
 
 namespace CourseApp.Tests
 {
@@ -8,7 +9,19 @@ namespace CourseApp.Tests
         [Fact]
         public void Test1()
         {
-            Assert.True(true);
+            Assert.Equal("22", CourseApp.Program.Plus(10, 12).ToString());
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            Assert.Equal("2", CourseApp.Program.Plus(1, 1).ToString());
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            Assert.Equal("20000", CourseApp.Program.Plus(10000, 10000).ToString());
         }
     }
 }
--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -7,9 +7,18 @@ namespace CourseApp
     {
         public static void Main(string[] args)
         {
-            BubbleSort.BubbleSortMethod();
+            // BubbleSort.BubbleSortMethod();
+            Console.WriteLine(Plus(10, 12));
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(a.GetType());
+        }
 
-            Console.WriteLine("Hello World");
+    public static int Plus(int a, int b)
+        {
+            /*int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());*/
+
+            return a + b;
         }
     }
 }
