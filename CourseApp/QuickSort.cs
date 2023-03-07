aaba1e396cc27632e4b19b3bc6a5b60f7da5d5b0
Fuflick <stepan_belov_2016@mail.ru>
2023-03-07 19:08:33 +0300

    Add QuickSort method

---
+++ CourseApp/QuickSort.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace CourseApp
+{
+    public class QuickSort
+    {
+        private int[] numbers = { 4, -1, 3421, 1234, -32, 12 };
+
+        public static int FindPivot(int[] numbers, int minIndex, int maxIndex) // поиск опроного члена
+        {
+            int pivot = minIndex - 1;
+            int temp;
+            for ( int i = minIndex; i < maxIndex; i++)
+            {
+                if (numbers[i] < numbers[maxIndex])
+                {
+                    pivot++;
+                    temp = numbers[pivot];
+                    numbers[pivot] = numbers[i];
+                    numbers[i] = temp;
+                }
+            }
+
+            pivot++;
+            temp = numbers[pivot];
+            numbers[pivot] = numbers[maxIndex];
+            numbers[maxIndex] = temp;
+
+            return pivot;
+        }
+
+        public int[] Sort(int[] nmbers, int minINdex, int maxIndex)
+        {
+            if (minINdex >= maxIndex)
+            {
+                return numbers;
+            }
+
+            int pivot = FindPivot(numbers, minINdex, maxIndex);
+
+            Sort(numbers, minINdex, pivot - 1);
+            Sort(nmbers, pivot + 1, maxIndex);
+
+            return numbers;
+        } // сама сортировка
+    }
+}
