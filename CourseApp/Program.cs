b53de3ba09081048d0ce7b006dfa7b017371eb29
Fuflick <stepan_belov_2016@mail.ru>
2023-03-09 11:14:51 +0300

    Test of my MergeSort alg

--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -7,9 +7,9 @@ namespace CourseApp
     {
         public static void Main(string[] args)
         {
-            int[] numbers = { 1, 2, 3, 4, -213, 12, -1, 5, 12 };
-            var sort = new QuickSort();
-            sort.Sort(numbers, 0, numbers.Length - 1);
+            int[] numbers = { 1, 4, 7, 22, 5 };
+            var mergeSort = new MergeSort();
+            mergeSort.Sort(numbers);
             foreach (int i in numbers)
             {
                 Console.WriteLine(i);
