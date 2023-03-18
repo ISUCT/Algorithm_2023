eca040904385530b9011541e8f78ff4f247d8c67
Demonage <tortoisegames431@gmail.com>
2023-03-19 00:12:12 +0300

    Solved task 5, wrote tests for it

--- golang/main.go
+++ golang/main.go
@@ -1,9 +1,5 @@
 package main
 
-import (
-	"isuct.ru/informatics2022/internal/module1"
-)
-
 func main() {
-	module1.SayHello()
+
 }
--- golang/tests/helpers/helper.go
+++ golang/tests/helpers/helper.go
@@ -19,3 +19,9 @@ func Replacer(input string, t *testing.T) (*os.File, *os.File) {
 	}
 	return r, w
 }
+
+func GetInputAndOutputFiles() (*os.File, *os.File) {
+	input, _ := os.OpenFile("input.txt", os.O_RDWR|os.O_TRUNC|os.O_CREATE, 0755)
+	output, _ := os.OpenFile("output.txt", os.O_RDONLY, 0755)
+	return input, output
+}
