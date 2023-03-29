08608bc944090403659de5f9c769df6192a3f6b5
Kraiben <stepan_belov_2016@mail.ru>
2023-03-29 21:29:45 +0300

    Add find Substring method by Rabin-Karp

--- CourseApp/Module3/FindString.cs
+++ CourseApp/Module3/FindString.cs
@@ -1,22 +1,57 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace CourseApp.Module3
 {
     public class FindString
     {
-        public static void FindStr()
+        public static long Get_hash(string str, int b, int q, int t)
         {
-            var a = Console.ReadLine();
-            var b = Console.ReadLine();
-            List<string> result = new List<string>();
-            for (int i = 0; ; i += b.Length)
+            long res = 0;
+            for (int i = 0; i < b; i++)
             {
-                result.Add(a.IndexOf(b, i).ToString());
+                res = ((res * t) + str[i]) % q;
             }
 
-            Console.WriteLine(result.Aggregate((d, c) => d + " " + c) + "\r\n");
+            return res;
+        }
+
+        public static void Rabin_karp(string e, string k, int o, int v)
+        {
+            long ht = Get_hash(k, k.Length, o, v);
+
+            long hs = Get_hash(e, k.Length, o, v);
+
+            long xt = 1;
+
+            for (int i = 0; i < k.Length; i++)
+            {
+                xt = (xt * v) % o;
+            }
+
+            for (int i = 0; i <= e.Length - k.Length; i++)
+            {
+                if (ht == hs)
+                {
+                    Console.Write("{0} ", i);
+                }
+
+                if (i + k.Length < e.Length)
+                {
+                    hs = ((hs * v) - (e[i] * xt) + e[i + k.Length]) % o;
+                    hs = (hs + o) % o;
+                }
+            }
+        }
+
+        public static void FindStr()
+        {
+            string str = Console.ReadLine();
+            string subStr = Console.ReadLine();
+
+            int o = 67953405;
+            int v = 26;
+
+            Rabin_karp(str, subStr, o, v);
         }
     }
 }
