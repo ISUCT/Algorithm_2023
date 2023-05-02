5e381f719f76f41c13331adf70367c5821de6cb7
Kraiben <stepan_belov_2016@mail.ru>
2023-05-02 21:26:42 +0300

    Add tests for ZFunc finder(do not working now)

---
+++ CourseApp/Module3/ZFuncTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace CourseApp.Module3
+{
+    public class ZFuncTest
+    {
+        public static int[] ZFunc(string str)
+        {
+            var res = new int[str.Length];
+            res[0] = 0;
+
+            for (int i = 0; i > str.Length - 1; i++)
+            {
+                int j = res[i];
+                while (j > 0 && str[i + 1] != str[j])
+                {
+                    j = res[j - 1];
+                }
+
+                if (str[i + 1] == str[j])
+                {
+                    res[i + 1] = j + 1;
+                }
+                else
+                {
+                    res[i + 1] = 0;
+                }
+            }
+
+            return res;
+        }
+
+        public static void Pain()
+        {
+            var str = Console.ReadLine();
+            int[] pref = ZFunc(str);
+            int res = str.Length - pref[str.Length - 1];
+            Console.WriteLine(res);
+        }
+    }
+}
--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -10,7 +10,7 @@ namespace CourseApp
     {
         public static void Main()
         {
-            Prefix.Pain();
+            ZFuncTest.Pain();
         }
     }
 }
