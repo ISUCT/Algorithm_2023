1e9956c2e701bde50945ae86405c0b42f3a3502e
Kraiben <stepan_belov_2016@mail.ru>
2023-05-03 23:44:00 +0300

    Start to write BalsCount method. (4 module 1 task) why stach is empty??? (((

---
+++ CourseApp/Module4/BalsCount.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CourseApp.Module4
+{
+    public class BalsCount
+    {
+        public static int Count(string nums)
+        {
+            var myStack = new Stack<int>();
+            var resStack = new Stack<int>();
+            var count = 1;
+            var res = 0;
+
+            for (int i = 2; i < nums.Length; i += 2)
+            {
+                myStack.Push(Convert.ToInt16(nums[i]));
+            }
+
+            var length = myStack.Count;
+
+            while (myStack.Count > 0 || res != length / 3)
+            {
+                resStack.Push(myStack.Pop());
+                if (resStack.Pop() == myStack.Peek())
+                {
+                    count++;
+                    if (count == 3)
+                    {
+                        res++;
+                        resStack.Clear();
+                    }
+                }
+                else
+                {
+                    resStack.Clear();
+                    count = 1;
+                }
+            }
+
+            return res * 3;
+        }
+    }
+}
--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using CourseApp.Module2;
 using CourseApp.Module3;
+using CourseApp.Module4;
 
 namespace CourseApp
 {
@@ -10,7 +12,8 @@ namespace CourseApp
     {
         public static void Main()
         {
-            ZFuncTest.Pain();
+            var i = BalsCount.Count("5 1 3 3 3 2");
+            Console.WriteLine(i);
         }
     }
 }
